82242ab699549ded7578f72c9685e6ca9464aad3
brogeby <brogeby@gmail.com>
2020-12-03 14:39:15 +0100

    Removed unnessecary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React-Notes</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={styles.heading}>Test 2</h1>
     </div>
   )
 }
 
 export default App
+
+const styles = {
+  heading: {
+    fontSize: 200,
+  },
+}
